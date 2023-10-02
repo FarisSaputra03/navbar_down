aaba95aadbbb601eef35970b63b11e29cd435111
Faris Saputra <farissaputra375@gmail.com>
2023-10-02 10:16:32 +0700

    update tambahan

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import Hero from "@/components/hero";
 import Contact from "@/components/contact";
+import Blog from "@/components/blog";
 export default function Home() {
   return (
    <>
     <Hero/>
     <Contact/>
+    <Blog/>
    </>
   );
 }
